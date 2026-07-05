379344e57299e3aff551fed7340d9e99d1410cac
AhmedTaher2002 <ahmed.taher.salah.1@gmail.com>
2026-07-05 06:10:02 +0300

    implement Calculate Matrics EndPoint

---
+++ FCEService/Features/CalculatedMetrics/Orchestrators/CalculatedMetricsOrchestrator.cs
@@ -0,0 +1,37 @@
+﻿using FCEService.Common.ViewModels;
+using FCEService.Domain.Enums;
+using FCEService.Domain.ValueObject;
+using MediatR;
+
+namespace FCEService.Features.CalculatedMetrics.Orchestrators
+{
+    public record CalculatedMetricsOrchestrator(int userId) : IRequest<RequestResult<CalculatedMetricsResponseDTO>>;
+    public class CalculatedMetricsOrchestratorHandler : IRequestHandler<CalculatedMetricsOrchestrator, RequestResult<CalculatedMetricsResponseDTO>>
+    {
+        private readonly IMediator _mediator;
+        public CalculatedMetricsOrchestratorHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<RequestResult<CalculatedMetricsResponseDTO>> Handle(CalculatedMetricsOrchestrator request, CancellationToken cancellationToken)
+        {
+            var queryResult = await _mediator.Send(new Queries.GetDetailsandCalculatedMetricsQuery(request.userId), cancellationToken);
+            if (!queryResult.IsSuccess)
+            {
+                return RequestResult<CalculatedMetricsResponseDTO>.Failure(queryResult.Message);
+            }
+            var calculatedMetrics = queryResult.Data;
+            await _mediator.Send(new Commands.CalculatedMetricsCommand(calculatedMetrics), cancellationToken);
+            var responseDTO = new CalculatedMetricsResponseDTO(
+                request.userId,
+                calculatedMetrics.BMR,
+                calculatedMetrics.TDEE,
+                calculatedMetrics.CalorieTarget,
+                calculatedMetrics.BMRRange,
+                calculatedMetrics.BMRStatus
+            );
+            return RequestResult<CalculatedMetricsResponseDTO>.Success(responseDTO, "Calculated metrics retrieved successfully.");
+        }
+    }
+    public record CalculatedMetricsResponseDTO(int UserId, double BMR, double TDEE, double CalorieTarget, BMRRange BMRRange, BMRStatus BMRStatus);
+}
---
+++ FCEService/Features/CalculatedMetrics/Queries/GetDetailsandCalculatedMetricsQuery.cs
@@ -0,0 +1,30 @@
+﻿using FCEService.Common.Abstract;
+using FCEService.Common.ViewModels;
+using FCEService.Domain.Enums;
+using FCEService.Domain.ValueObject;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FCEService.Features.CalculatedMetrics.Queries
+{
+    public record GetDetailsandCalculatedMetricsQuery(int userId) : IRequest<RequestResult<FCEService.Domain.Aggregates.CalculatedMetrics>>;
+    public class GetDetailsandCalculatedMetricsQueryHandler : IRequestHandler<GetDetailsandCalculatedMetricsQuery, RequestResult<FCEService.Domain.Aggregates.CalculatedMetrics>>
+    {
+        private readonly IGenericRepository<FCEService.Domain.Aggregates.UserFitnessStats> _repository;
+        public GetDetailsandCalculatedMetricsQueryHandler(IGenericRepository<FCEService.Domain.Aggregates.UserFitnessStats> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<RequestResult<FCEService.Domain.Aggregates.CalculatedMetrics>> Handle(GetDetailsandCalculatedMetricsQuery request, CancellationToken cancellationToken)
+        {
+            var userFitnessStats = await _repository.Get(x => x.userId == request.userId && x.IsActive).FirstOrDefaultAsync();
+            var calculatedMetrics = FCEService.Domain.Aggregates.CalculatedMetrics.Calculate(userFitnessStats);
+            if (calculatedMetrics == null)
+            {
+                return RequestResult<FCEService.Domain.Aggregates.CalculatedMetrics>.Failure("Calculated metrics not found.");
+            }
+            return RequestResult<FCEService.Domain.Aggregates.CalculatedMetrics>.Success(calculatedMetrics, "Calculated metrics retrieved successfully.");
+        }
+    }
+
+}
