d06f068bad5e7c649abf54cedd9529bdebc985b9
AhmedTaher2002 <ahmed.taher.salah.1@gmail.com>
2026-07-07 06:31:43 +0300

    assign user plan history endpoint

--- FCEService/Domain/Entities/UserPlanHistory.cs
+++ FCEService/Domain/Entities/UserPlanHistory.cs
@@ -2,8 +2,9 @@
 {
     public class UserPlanHistory : BaseEntity
     {
-        public Guid UserId { get; set; }
+        public int UserId { get; set; }
         public int ExternalPlanId { get; set; }
+        public DateTime AssignedAt { get; set; }
         public DateTime? EndedAt { get; set; }
         public string? ResonForChange { get; set; }
     }
---
+++ FCEService/Features/AssignUserPlanHistory/commands/AssignUserPlanHistorycommand.cs
@@ -0,0 +1,33 @@
+﻿using FCEService.Common.Abstract;
+using FCEService.Common.ViewModels;
+using MediatR;
+
+namespace FCEService.Features.AssignUserPlanHistory.commands
+{
+    public record AssignUserPlanHistorycommand(int UserId, int ExternalPlanId, DateTime? AssignedAt, DateTime? EndedAt, string? ReasonForChange) : IRequest<RequestResult<bool>>;
+    public class AssignUserPlanHistorycommandHandler : IRequestHandler<AssignUserPlanHistorycommand, RequestResult<bool>>
+    {
+        private readonly IGenericRepository<Domain.Entities.UserPlanHistory> _repository;
+        public AssignUserPlanHistorycommandHandler(IGenericRepository<Domain.Entities.UserPlanHistory> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<RequestResult<bool>> Handle(AssignUserPlanHistorycommand request, CancellationToken cancellationToken)
+        {
+            var assignedAt = request.AssignedAt ?? DateTime.UtcNow;
+            var userPlanHistory = new Domain.Entities.UserPlanHistory
+            {
+                UserId = request.UserId,
+                ExternalPlanId = request.ExternalPlanId,
+                AssignedAt = assignedAt,
+                EndedAt = request.EndedAt,
+                ResonForChange = request.ReasonForChange
+            };
+            _repository.Add(userPlanHistory);
+            await _repository.SaveChangeAsync(cancellationToken);
+            return RequestResult<bool>.Success(true, "User plan history assigned successfully.");
+        }
+    }
+
+
+}
