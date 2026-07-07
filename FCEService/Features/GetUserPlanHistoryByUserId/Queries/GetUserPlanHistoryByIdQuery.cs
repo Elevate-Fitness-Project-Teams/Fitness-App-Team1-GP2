64cf5419dcd7d40f79ee7d5a3d1cee6de400c74f
AhmedTaher2002 <ahmed.taher.salah.1@gmail.com>
2026-07-07 07:31:35 +0300

    implement get userplan history by user id

---
+++ FCEService/Features/GetUserPlanHistoryByUserId/Queries/GetUserPlanHistoryByIdQuery.cs
@@ -0,0 +1,36 @@
+﻿using FCEService.Common.Abstract;
+using FCEService.Common.ViewModels;
+using FCEService.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FCEService.Features.GetUserPlanHistoryByUserId.Queries
+{
+    public record GetUserPlanHistoryByIdQuery(int userId) : IRequest<RequestResult<List<UserPlanHistoryDTO>>>;
+
+    public class GetUserPlanHistoryByIdQueryHandler : IRequestHandler<GetUserPlanHistoryByIdQuery, RequestResult<List<UserPlanHistoryDTO>>>
+    {
+        private IGenericRepository<UserPlanHistory> _repository;
+        public GetUserPlanHistoryByIdQueryHandler(IGenericRepository<UserPlanHistory> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<RequestResult<List<UserPlanHistoryDTO>>> Handle(GetUserPlanHistoryByIdQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _repository.Get(x => x.UserId == request.userId).AsNoTracking().Select(x => new UserPlanHistoryDTO(
+                x.UserId,
+                x.ExternalPlanId,
+                x.AssignedAt,
+                x.EndedAt,
+                x.ResonForChange
+            )).ToListAsync(cancellationToken);
+            if (result == null)
+            {
+                return RequestResult<List<UserPlanHistoryDTO>>.Failure("User plan history not found for the specified user.");
+            }
+            
+            return RequestResult<List<UserPlanHistoryDTO>>.Success(result, "User plan history retrieved successfully.");
+        }
+    }
+    public record UserPlanHistoryDTO(int UserId, int ExternalPlanId, DateTime AssignedAt, DateTime? EndedAt, string? ResonForChange);
+}
