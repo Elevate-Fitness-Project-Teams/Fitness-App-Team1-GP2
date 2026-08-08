458c8b077c028009356dd88d6f737901719f1ac8
Mohamed Elganzory <mohamedelganzory621@gmail.com>
2026-08-08 15:39:52 +0300

    Common.Security

---
+++ FitnessApp/Common/Security/DevelopmentAuthHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+
+namespace FitnessApp.Common.Security;
+
+public sealed class DevelopmentAuthHandler
+    : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    public const string SchemeName = "Development";
+
+    public DevelopmentAuthHandler(
+        IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder)
+        : base(options, logger, encoder)
+    {
+    }
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, "10"),
+            new Claim(ClaimTypes.Name, "Development User")
+        };
+
+        var identity = new ClaimsIdentity(claims, SchemeName);
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, SchemeName);
+
+        return Task.FromResult(AuthenticateResult.Success(ticket));
+    }
+}
